38f5c83417c724cffa7e979d5cda5f41abaad0de
Bob <dorafmon@gmail.com>
2020-04-19 10:50:56 +0000

    checkpoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ export NODE_OPTIONS=--max-old-space-size=4096
 /opt/conda/envs/dev/bin/jupyter labextension install jupyterlab-plotly@1.5.0 --no-build
 /opt/conda/envs/dev/bin/jupyter labextension install plotlywidget@1.5.0 --no-build
 /opt/conda/envs/dev/bin/jupyter lab build
-/opt/conda/envs/dev/bin/pip install alpha_vantage
+/opt/conda/envs/dev/bin/pip install alpha_vantage newsapi-python
 unset NODE_OPTIONS
 /opt/conda/envs/dev/bin/jupyter lab --allow-root --ip="0.0.0.0" --notebook-dir="/notes" --NotebookApp.password="sha1:485be2efd10b:a1b93081ee7019e639fbab08c6d3005f229d386c"
 
