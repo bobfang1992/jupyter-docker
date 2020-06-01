630cf8f80dbc62538b479cf5697cb58888241735
Bob Fang <bob.fang.london@gmail.com>
2020-06-01 10:02:23 +0000

    Add dask

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,10 @@
-FROM continuumio/miniconda
+FROM frolvlad/alpine-miniconda3:python3.7
 
-RUN apt-get update -yq \
-    && apt-get install curl gnupg -yq python3-tk ghostscript\
-    && curl -sL https://deb.nodesource.com/setup_10.x | bash \
-    && apt-get install nodejs -yq
+RUN apk add --update nodejs nodejs-npm
+
 RUN conda create -n dev python=3.7 \
-	&& conda install -n dev nomkl jupyter pandas seaborn scikit-learn\
-    && conda install -n dev -c conda-forge jupyterlab redis-py plotly arctic cufflinks-py camelot-py\
+	&& conda install -n dev nomkl pandas dask seaborn scikit-learn\
+    && conda install -n dev -c conda-forge jupyterlab redis-py plotly arctic cufflinks-py camelot-py "ipywidgets=7.5"\
     && conda clean -afy\
     && mkdir -p /notes
 COPY entrypoint.sh /opt/
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
-
-export NODE_OPTIONS=--max-old-space-size=4096
-/opt/conda/envs/dev/bin/jupyter labextension install @jupyter-widgets/jupyterlab-manager@1.1 --no-build
-/opt/conda/envs/dev/bin/jupyter labextension install jupyterlab-plotly@1.5.0 --no-build
-/opt/conda/envs/dev/bin/jupyter labextension install plotlywidget@1.5.0 --no-build
-/opt/conda/envs/dev/bin/jupyter lab build
+alias jupyter="/opt/conda/envs/dev/bin/jupyter"
 /opt/conda/envs/dev/bin/pip install alpha_vantage newsapi-python newscatcher
-unset NODE_OPTIONS
-/opt/conda/envs/dev/bin/jupyter lab --allow-root --ip="0.0.0.0" --notebook-dir="/notes" --NotebookApp.password="sha1:485be2efd10b:a1b93081ee7019e639fbab08c6d3005f229d386c"
+jupyter labextension install jupyterlab-plotly@4.7.1
+jupyter labextension install @jupyter-widgets/jupyterlab-manager plotlywidget@4.7.1
+jupyter lab --allow-root --ip="0.0.0.0" --notebook-dir="/notes" --NotebookApp.password="sha1:485be2efd10b:a1b93081ee7019e639fbab08c6d3005f229d386c"
 
