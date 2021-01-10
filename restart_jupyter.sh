465203a566d261788fc03bb1d8feeef321f7c9fe
Bob Fang <bob.fang.london@gmail.com>
2021-01-10 22:50:34 +0000

    update restart

--- restart_jupyter.sh
+++ restart_jupyter.sh
@@ -1,5 +1,5 @@
 set -e
-
+docker build -t dev:latest .
 docker stop jupyter_lab
 docker rm jupyter_lab
 docker run -it --name jupyter_lab --network=host --mount src="/home/bfang/notes",target="/notes",type=bind -d --restart=always dev:latest
